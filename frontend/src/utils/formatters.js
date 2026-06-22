ab011097b37b7fe6b57bf3c0293fc5fb98833097
Kshitij Kanse <kshitijkanse20042004@rjcollege.edu.in>
2026-06-22 10:29:09 +0530

    first message

--- frontend/src/utils/formatters.js
+++ frontend/src/utils/formatters.js
@@ -2,7 +2,7 @@
 export const formatCurrency = (amount) => {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
-    currency: "USD",
+    currency: "INR",
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
   }).format(amount);
